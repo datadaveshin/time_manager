16dbca7c808fa0987bce63af567ead3bd9920cce
David Shin <davidshin444@gmail.com>
2020-03-25 15:47:01 -0700

    Finish-setting-up-static-homepage: Added route, CV and test

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,7 +4,16 @@ describe 'visit' do
   describe 'home' do
     it 'can be reached successfully' do
       visit root_path
-      expect(page_status_code).to eq(200)
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
+
+feature "Page Header" do
+  scenario "has home content" do
+    visit root_path
+    within('h1') do
+      expect(page).to have_content("Home")
     end
   end
 end
