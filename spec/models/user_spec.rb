0ac826a2bc61211864aca92c3941174e613b5ac3
David Shin <davidshin444@gmail.com>
2020-03-25 18:09:27 -0700

    Added User tests and code to create and validate User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    before do
+      @user = User.create(email: "test@example.com", password:"asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
+    end
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+    it "cannot be created without first_name, last_name" do
+      @user.first_name = nil
+      expect(@user.first_name).to be_falsey
+    end
+    it "cannot be created without last_name" do
+      @user.last_name = nil
+      expect(@user.last_name).to be_falsey
+    end
+  end
+  after(:all) { User.destroy_all }
 end
